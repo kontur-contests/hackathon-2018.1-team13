8ea417952bb03e930b4b2c3ef19593fcd8475c30
Suhvan <suhvan4ik@gmail.com>
2018-04-07 19:29:20 +0500

    drones

--- Hackaton 2018/Assets/Scripts/Enemies/Drone.cs
+++ Hackaton 2018/Assets/Scripts/Enemies/Drone.cs
@@ -15,6 +15,9 @@ public class Drone : MonoBehaviour, IHitable
     [SerializeField]
     float forceAdjStep = 1;
 
+    public float hoverForce = 5.0F;
+    public float hoverDamp = 0.5F;
+
     [SerializeField]
     float lookAtSpeed = 50;
 
@@ -33,6 +36,9 @@ public class Drone : MonoBehaviour, IHitable
     [SerializeField]
     float maxAngularVelocity = 50;
 
+    [SerializeField]
+    public float hoverHeight = 4;
+
 
 
     protected Rigidbody ragdoll;
@@ -46,37 +52,66 @@ public class Drone : MonoBehaviour, IHitable
 
     // Use this for initialization
     void Start () {
-		
+        transform.LookAt(aimTarget);
 	}
 
     void FixedUpdate()
+    {        
+        HeightStabilization();       
+        AngularStabilization();
+        MoveToTarget();
+    }
+
+
+
+    void AngularStabilization()
     {
-        //height stabilization
-        curForce -= forceAdjStep * ragdoll.velocity.y;
-        ragdoll.AddForce(Vector3.up * Time.deltaTime * curForce, ForceMode.Impulse);
-
-
-        //angularVelocity stabilization
         var targetAngVel = Vector3.Lerp(ragdoll.angularVelocity, Vector3.zero, Time.deltaTime * angularStability);
         ragdoll.angularVelocity = targetAngVel;
+    }
 
-        //moving to target
+    private void HeightStabilization()
+    {
+        //curForce -= forceAdjStep * ragdoll.velocity.y;
+        //ragdoll.AddForce(Vector3.up * Time.deltaTime * curForce, ForceMode.Impulse);
+
+        RaycastHit hit;
+        Ray downRay = new Ray(transform.position, -Vector3.up);
+        if (Physics.Raycast(downRay, out hit))
+        {
+            float hoverError = hoverHeight - hit.distance;
+            if (hoverError > 0)
+            {
+                float upwardSpeed = ragdoll.velocity.y;
+                float lift = hoverError * hoverForce - upwardSpeed * hoverDamp;
+                ragdoll.AddForce(Mathf.Clamp( lift, -curForce, curForce) * Vector3.up);
+            }
+        }
+
+
+    }
+
+    void MoveToTarget()
+    {
+        //Don't move on hit
         Vector3 relativePos = moveTarget.position - gameObject.transform.position;
         var vel = ragdoll.velocity;
         var sqDisToRange = relativePos.sqrMagnitude - targetDistanceSqr;
 
         var targetLinVel = Vector3.Lerp(ragdoll.velocity, relativePos.normalized * sqDisToRange, Time.deltaTime * moveForce);
 
-        ragdoll.velocity = new Vector3(targetLinVel.x, Mathf.Clamp( vel.y, -speedClamp, speedClamp), targetLinVel.z);
+        //ragdoll.velocity = new Vector3(Mathf.Clamp(targetLinVel.y, -speedClamp, speedClamp), vel.y, Mathf.Clamp(targetLinVel.z, -speedClamp, speedClamp));
+        //ragdoll.velocity = new Vector3(targetLinVel.x, Mathf.Clamp( vel.y, -speedClamp, speedClamp), targetLinVel.z);
+        ragdoll.velocity = new Vector3(targetLinVel.x, vel.y, targetLinVel.z);
     }
-      
 
-	void Update ()
+
+    void Update ()
     {
         //aim
-      var rotStep = Time.deltaTime * lookAtSpeed;
-    var targetRot = Quaternion.LookRotation(aimTarget.position - transform.position);
-    transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRot, rotStep);
+        var rotStep = Time.deltaTime * lookAtSpeed;
+        var targetRot = Quaternion.LookRotation(aimTarget.position - transform.position);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRot, rotStep);
         
     }
 
